7b5adeadd16e641ca404f6dab869f9df89c611ba
LimeYun <jstar3183@naver.com>
2025-01-24 09:58:45 +0900

    투두리스트

---
+++ todo-app/src/components/TodoInput.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const TodoInput = ({ input, onChange, onSubmit }) => {
+    return (
+        <div>
+            <form action="" className='form' onSubmit={onSubmit}>
+                <input type="text" placeholder='할 일 입력' className='input' value={input} onChange={onChange} />
+                <button type="submit" className='btn'>추가</button>
+            </form>
+        </div>
+    )
+}
+
+export default TodoInput
